4acebc70b8fecc08df4aaa2a3b1273bf12940c4c
BK <bkam03@yahoo.com>
2017-05-27 15:02:32 -1000

    erase, clear, color selection working

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -22,10 +22,24 @@ function pixelPainter() {
       appendTarget.appendChild(row);
     }
   }
+
+  function clear() {
+    var clearGrid = document.querySelectorAll('.pixelSq');
+    for( var i = 0; i < clearGrid.length; i++ ) {
+      clearGrid[i].style.background = "white";
+    }
+  }
+
+  function erase() {
+    setCurrentColor('white');
+  }
+
   return {
     getCurrentColor : getCurrentColor,
     setCurrentColor : setCurrentColor,
-    makeGrid : makeGrid
+    makeGrid : makeGrid,
+    clear : clear,
+    erase : erase
   };
 }
 
@@ -49,36 +63,45 @@ function pixelPainter() {
     }
   }
 
-  function clickPixel() {  //
-    this.style.background = "orange";
+  function clickPixel() {  //change this so color picking matters.  problem is how to access pixel painter object from here
+    this.style.background = pixelPainter1.getCurrentColor();
 
   }
 
   function pickColor() { //when you click on a color tile, it makes that the new drawing color.
-
+    pixelPainter1.setCurrentColor(this.style.background);
   }
 
 
+  function fillColorPalette(arrayOfColors){  //fills the color pixels with ...color
+    var colorGrid = document.querySelectorAll('.colorSq');
+    console.log(colorGrid);
+    for( var i = 0; i < arrayOfColors.length; i++ ) {
+      console.log(i);
+      colorGrid[i].style.background = arrayOfColors[i];
+    }
+  }
 
-
+  var colorArray = [ 'black', '#373737', '#D3D3D3', 'orange' ];  //set your color choice here.
 
   var pixelPainter1 = pixelPainter();
 
   makeDiv('grid', 'div', mainBox, 'grid');
   makeDiv('sideBar', 'div', mainBox, 'sideBar');
   makeDiv('colors', 'div', sideBar, 'colors');
-  makeDiv('eraseButton', 'div', sideBar, 'eraseButton');
-  makeDiv('clearButton', 'div', sideBar, 'clearButton');
+  makeDiv('eraseButton', 'button', sideBar, 'eraseButton');
+  makeDiv('clearButton', 'button', sideBar, 'clearButton');
 
   pixelPainter1.makeGrid(3, 3, 'pixelSq',grid); //set grid size here
   eventListeners('.pixelSq', clickPixel);
 
   pixelPainter1.makeGrid(2, 2,'colorSq', colors);  //set color palette size here
   eventListeners('.colorSq', pickColor);
+  fillColorPalette(colorArray);
 
+  eventListeners('#clearButton', pixelPainter1.clear);
 
-
-
+  eventListeners('#eraseButton', pixelPainter1.erase);
 
 
 
