a50d000b51a602cdda2cc7cfc2a866aa8742e588
BK <bkam03@yahoo.com>
2017-05-27 19:07:43 -1000

    partially working drag function

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,5 +1,6 @@
 function pixelPainter() {
   var currentColor = "black";
+  var isDragging = false;
 
   function getCurrentColor() {
     return currentColor;
@@ -7,6 +8,14 @@ function pixelPainter() {
 
   function setCurrentColor(newColor) {
     currentColor = newColor;
+  }
+
+  function getDragging() {
+    return isDragging;
+  }
+
+  function setDragging(value) {
+    isDragging = value;
   }
 
   function makeGrid(width, height, pixelClass, appendTarget) {
@@ -33,18 +42,20 @@ function pixelPainter() {
     setCurrentColor('white');
   }
 
+
   return {
     getCurrentColor : getCurrentColor,
     setCurrentColor : setCurrentColor,
     makeGrid : makeGrid,
     clear : clear,
-    erase : erase
+    erase : erase,
+    getDragging : getDragging,
+    setDragging : setDragging
   };
 }
 
 (function(){
 
-  var mainBox = document.querySelector('#pixelPainter');
 
   function makeDiv(id, type, append, content) {
     var newElement = document.createElement(type);
@@ -61,7 +72,14 @@ function pixelPainter() {
   }
 
   function clickPixel() {  //change this so color picking matters.  problem is how to access pixel painter object from here
-    this.style.background = pixelPainter1.getCurrentColor();
+      this.style.background = pixelPainter1.getCurrentColor();
+  }
+  function dragPixel() {  //change this so color picking matters.  problem is how to access pixel painter object from here
+    console.log("clickpixel " + pixelPainter1.getDragging());
+    if( pixelPainter1.getDragging() === true ) {
+      console.log("painting");
+      this.style.background = pixelPainter1.getCurrentColor();
+    }
 
   }
 
@@ -77,11 +95,25 @@ function pixelPainter() {
     }
   }
 
+  function nowDragging() {
+    console.log("now dragging");
+    pixelPainter1.setDragging(true);
+    console.log(pixelPainter1.getDragging());
+  }
+
+  function notDragging() {
+    console.log("not dragging");
+    pixelPainter1.setDragging(false);
+  }
+
+
+
   var colorArray = [ '#000000', '#333333', '#555555', '#777777','#ffffff', '#00059F', '#0229BF', '#4E91FD', '#78C4FD', '#B9E2F5', '#006400', '#008B00', '#00CD00', '#00EE00', '#95F571', '#660066', '#800080', '#BE29EC', '#D896FF', '#EFBBFF', '#6F0000', '#B80000', '#D22525', '#FF6666', '#FFB9B9', '#FF4F00', '#FF6700', '#FF9248', '#FFAC00', '#FFE577', '#2E1915', '#663300', '#996633', '#BB9966', '#FFCC99'];  //set your color choice here.
 
   var pixelPainter1 = pixelPainter();
 
 
+  var mainBox = document.querySelector('#pixelPainter');
   makeDiv('sideBar', 'div', mainBox, '');
   makeDiv('colors', 'div', sideBar, '');
   makeDiv('eraseButton', 'button', sideBar, 'eraseButton');
@@ -91,6 +123,14 @@ function pixelPainter() {
   pixelPainter1.makeGrid(20, 20, 'pixelSq',grid); //set grid size here
   eventListeners('.pixelSq', clickPixel);
 
+    var currentClass = document.querySelectorAll('.pixelSq');
+    for (var i = 0; i < currentClass.length; i++) {
+      currentClass[i].addEventListener('mouseover', dragPixel);
+    }
+
+
+
+
   pixelPainter1.makeGrid(7, 5,'colorSq', colors);  //set color palette size here
   eventListeners('.colorSq', pickColor);
   fillColorPalette(colorArray);
@@ -98,6 +138,10 @@ function pixelPainter() {
   eventListeners('#clearButton', pixelPainter1.clear);
 
   eventListeners('#eraseButton', pixelPainter1.erase);
+
+  grid.addEventListener("mousedown", nowDragging);
+  grid.addEventListener("mouseup", notDragging);
+
 
 
 
