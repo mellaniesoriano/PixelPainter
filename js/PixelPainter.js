d4a87d82dd1ac250b1161501254fc4963e43c790
BK <bkam03@yahoo.com>
2017-05-29 11:15:46 -1000

    incomplete save function, issue with getting background color of pixel into variable

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -2,6 +2,8 @@ function pixelPainter() {
   var currentColor = "black";
   var isDragging = false;
 
+  var saveFile = {};
+
   function getCurrentColor() {
     return currentColor;
   }
@@ -42,6 +44,15 @@ function pixelPainter() {
     setCurrentColor('white');
   }
 
+  function save() {
+    var getWholeGrid = document.querySelectorAll(".pixelSq");
+    var savingPixels = [];
+    var currentPixelColor = "";
+    for( var i = 0; i < getWholeGrid.length; i++ ) {
+      currentPixelColor = getWholeGrid[i].style;
+    }
+    console.log(getWholeGrid[1].style);
+  }
 
   return {
     getCurrentColor : getCurrentColor,
@@ -50,7 +61,8 @@ function pixelPainter() {
     setDragging : setDragging,
     makeGrid : makeGrid,
     clear : clear,
-    erase : erase
+    erase : erase,
+    save : save
   };
 }
 
@@ -106,6 +118,7 @@ function pixelPainter() {
 
 
 
+
   var colorArray = [ '#000000', '#333333', '#555555', '#777777','#ffffff', '#00059F', '#0229BF', '#4E91FD', '#78C4FD', '#B9E2F5', '#006400', '#008B00', '#00CD00', '#00EE00', '#95F571', '#660066', '#800080', '#BE29EC', '#D896FF', '#EFBBFF', '#6F0000', '#B80000', '#D22525', '#FF6666', '#FFB9B9', '#FF4F00', '#FF6700', '#FF9248', '#FFAC00', '#FFE577', '#2E1915', '#663300', '#996633', '#BB9966', '#FFCC99'];  //set your color choice here.
 
   var pixelPainter1 = pixelPainter();
@@ -113,12 +126,14 @@ function pixelPainter() {
 
   var mainBox = document.querySelector('#pixelPainter');
   makeDiv('sideBar', 'div', mainBox, '');
+  makeDiv('grid', 'div', mainBox, '');
+
   makeDiv('colors', 'div', sideBar, '');
   makeDiv('sideBarChild', 'div', sideBar, '');
   makeDiv('curColorBox', 'div', sideBarChild, '');
-  makeDiv('eraseButton', 'button', sideBarChild, 'eraseButton');
-  makeDiv('clearButton', 'button', sideBarChild, 'clearButton');
-  makeDiv('grid', 'div', mainBox, '');
+  makeDiv('eraseButton', 'button', sideBarChild, 'Erase');
+  makeDiv('clearButton', 'button', sideBarChild, 'Clear');
+  makeDiv('saveButton', 'button', sideBarChild, 'Save');
 
   pixelPainter1.makeGrid(20, 20, 'pixelSq',grid); //set grid size here
   eventListeners('.pixelSq', clickPixel);
@@ -136,8 +151,10 @@ function pixelPainter() {
   fillColorPalette(colorArray);
 
   eventListeners('#clearButton', pixelPainter1.clear);
-
   eventListeners('#eraseButton', pixelPainter1.erase);
+  eventListeners('#saveButton', pixelPainter1.save);
+
+
 
 
 
